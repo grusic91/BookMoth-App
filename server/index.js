afb2df5188dd7388fb384cf6d3994939b88a7e97
Gruzic <28716217+grusic91@users.noreply.github.com>
2019-11-12 14:30:29 +0100

    Adds fake-db with books, adds book model, push datas from fake-db to mongo db

--- server/index.js
+++ server/index.js
@@ -2,14 +2,27 @@ const express = require("express");
 const mongoose = require("mongoose");
 
 const config = require("./config/dev");
+const FakeDb = require("./fake-db");
 const errorHandler = require("./handler/error");
+const Book = require("./models/book");
 const PORT = process.env.PORT || 3005;
 
+// useful to see the acutal Mongo queries that are being run in terminal
+mongoose.set("debug", true);
 
+// important for using async functions
+mongoose.Promise = Promise;
+
+// connect function returns Promise, so we can call on it then
 mongoose.connect(config.DB_URI, {
+  keepAlive: true, // make sure that connection is stabel
   useNewUrlParser: true,
   useUnifiedTopology: true,
-});
+})
+  .then(() => {
+    const fakeDb = new FakeDb();
+    //fakeDb.seedDb();
+  });
 
 const app = express();
 // Routes come here
