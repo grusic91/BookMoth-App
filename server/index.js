d6f5a61196253771695fc6381c127878b54c3016
Gruzic <28716217+grusic91@users.noreply.github.com>
2019-11-12 12:49:33 +0100

    Create mongoDB, connect mongoDB with monogoose and app.

--- server/index.js
+++ server/index.js
@@ -1,8 +1,22 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
+
+const config = require("./config/dev");
 const errorHandler = require("./handler/error");
 const PORT = process.env.PORT || 3005;
 
+
+mongoose.connect(config.DB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const app = express();
+// Routes come here
+
+app.get("/books", function(req,res) {
+  res.json({"success": true});
+});
 
 /*Error handler if the route cann not be reached*/
 app.use(function(req, res, next) {
